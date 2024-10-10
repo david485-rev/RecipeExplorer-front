4a665c8653a85bc5e1ee8824f2462508160691ed
David Ho <davidho6308493@gmail.com>
2024-10-10 10:41:44 -0700

    basic proj structure to work off of

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<></>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
