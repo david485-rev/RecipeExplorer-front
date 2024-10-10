57634abb294ff9ccc3dd15a229c90a7f5c4d857a
Ian-Hoole <hoolei@sonoma.edu>
2024-10-10 13:12:03 -0700

    created login page with basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-
+import Login from "./components/login/login";
+import MockHome from "./components/mockHomePage/mockHome";
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<></>}/>
+        <Route path="/" element={<MockHome />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
