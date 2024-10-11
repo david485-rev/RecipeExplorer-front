e3965789d8b001749274e00ec8cd69f08b8be6aa
david485-rev <david485@revature.net>
2024-10-11 13:22:01 -0700

    Register page (#1)
    * able to register a new user and it shows on DB
    
    * a message now displays if register was successful or failed after submitting register info
    
    * register backend done
    
    * using bootstrap for form
    
    * changed email input type to email
    
    ---------
    
    Co-authored-by: David Ho <davidho6308493@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import RegisterContainer from './components/Register/RegisterContainer';
 
-function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<></>}/>
-      </Routes>
-    </div>
-  );
+function App(): JSX.Element {
+    return (
+        <div className='App'>
+            <Routes>
+                <Route path="/register" element={<RegisterContainer/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
