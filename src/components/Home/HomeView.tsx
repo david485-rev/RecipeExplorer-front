b0df3b726b3e4708340c223bdcfe45cb368a537c
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-14 07:40:28 -0700

    Home View component styling

--- src/components/Home/HomeView.tsx
+++ src/components/Home/HomeView.tsx
@@ -9,8 +9,15 @@ function HomeView({recipeIndex, recipesArr, skipRecipe}: HomeProps) {
       {recipesArr ? (
         <>
       <h2>{recipesArr[recipeIndex].recipeName}</h2>
+      <div className='home-info_wrapper'>
+        <p><span id='cuisine'>Cuisine:&nbsp;{recipesArr[recipeIndex].cuisine}</span><span id="pipe">&#10072;</span><span id='category'>Category:&nbsp;{recipesArr[recipeIndex].category}</span></p>
+      </div>
       <div className='home-image_wrapper'>
         <img id="recipe-image" src={recipesArr[recipeIndex].recipeThumb} alt={recipesArr[recipeIndex].recipeName} />
+      </div>
+      <div className='home-desc_wrapper'>
+        <h4>Description:</h4>
+        <p id="description">{recipesArr[recipeIndex].description ?? "No description"}</p>
       </div>
       <div className='buttons-wrapper'>
         <button id="left-arrow" onClick={skipRecipe}>&laquo;</button>
