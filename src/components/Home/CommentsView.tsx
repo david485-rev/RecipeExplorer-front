48e9ef0a720c2cff4e23a073cad33191541171c4
Burak <burakadmr@gmail.com>
2024-10-15 06:24:52 -0700

    Comments visibility (#10)
    * Restricting rating value to one decimal place if not a whole number
    
    * Logic to toggle visibility of a recipe's comments

--- src/components/Home/CommentsView.tsx
+++ src/components/Home/CommentsView.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import "../../styles/Home/CommentsView.css";
 import {RecipeComment } from './HomeController'
 
-type CommentsProps = {comments: RecipeComment[] | undefined}
+type CommentsProps = {comments: RecipeComment[] | undefined, isVisible: boolean}
 
-function CommentsView({comments}: CommentsProps) {
+function CommentsView({comments, isVisible}: CommentsProps) {
   return (
-    <div className='comments-wrapper'>
+    <div className={`comments-wrapper ${isVisible ? "" : "hidden"}`}>
     {comments && comments.length ? (comments.map((comment) => (
       (<p className="recipe-comment" key={comment.uuid}>{comment.description}</p>)
       ))) : <p>No comments</p>
