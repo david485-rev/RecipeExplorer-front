6f6eb8ba5339c0fc6adb0cb40571b27de50aa90d
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-15 14:29:52 -0700

    Implemented logic to create a recipe

--- src/components/CreateRecipe/CreateRecipeController.tsx
+++ src/components/CreateRecipe/CreateRecipeController.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import CreateRecipeView from './CreateRecipeView'
+import { UserContext } from '../Context/UserContext'
+import config from '../../config';
+const URL = `${config.path}`;
 
 function CreateRecipeController() {
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  const user = useContext(UserContext);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const form = e.currentTarget;
+      const formData = new FormData(form);
+      const formJson = Object.fromEntries(formData.entries());
+
+      if (Object.values(formJson).includes("")) {
+        setMessage("All fields are required")
+      } else {
+        createRecipe(formJson);
+      }
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  async function createRecipe(recipe: Object) {
+    const payload = {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${user?.token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(recipe)
+    }
+
+    try {
+      const recipeResponse = await fetch(`${URL}/recipes`, payload);
+      
+      if (recipeResponse.ok) {
+        setSubmitted(true)
+      }
+    } catch(err) {
+      setMessage("Failed to submit recipe, try again")
+      console.error(err)
+    }
+  }
+
   return (
-    <div>CreateRecipeController</div>
+    <CreateRecipeView submitForm={handleSubmit} formMessage={message} submitted={submitted}/>
   )
 }
 
