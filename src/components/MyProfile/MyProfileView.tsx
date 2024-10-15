0ee7b33798b5f77a097f509d4737b180f0e00013
Eugene Na <eugene486@revature.net>
2024-10-15 15:15:11 -0500

    stlying my profile page

--- src/components/MyProfile/MyProfileView.tsx
+++ src/components/MyProfile/MyProfileView.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios';
 import {UserContext} from '../Context/UserContext';
+import "../../styles/MyProfile/MyProfileView.css";
 export type ProfileInputType = {
     username: string,
     email: string,
@@ -38,31 +39,37 @@ function MyProfileView(props: any) {
     }, [])
 
     return (
-        <>
-        
-            <form onSubmit={submitHandler}>
-                <h1>My Profile</h1>
-                <img src = {userInput.picture} alt = "my image" width="100" height="100" /> 
-                <br/>
-                <input type="string" value = {userInput.picture} onChange={(event: any) => setUserInput({...userInput, picture: event.target.value})}/>
-                <br/>
-                <label htmlFor="username">Username:</label>
-                <br/>
-                <input type="text" value={userInput.username} onChange={(event: any) => setUserInput({...userInput, username: event.target.value})}/>
-                <br/>
-                <label htmlFor="email">Email:</label>
-                <br/>
-                <input type="text" value={userInput.email} onChange={(event: any) => setUserInput({...userInput, email: event.target.value})}/>
-                <br/>
-                <label htmlFor="description">Description:</label>
-                <br/>
-                <input type="text" value = {userInput.description} onChange={(event: any) => setUserInput({...userInput, description: event.target.value})}/>
-                <br/>
-                <label htmlFor="email">Email:</label>
-                <br/>
-                <h6>{message}</h6>
-                <button onClick={submitHandler}>Submit</button>
-            </form>
+        <>  
+            <div className="container mt-5">
+                <div className="row d-flex justify-content-center" >
+                    <div className="col-md-7">
+                        <div className = "card p-3 py-4">
+                            <form onSubmit={submitHandler}>
+                                <h1>My Profile</h1>
+                                <img src = {userInput.picture} alt = "my image" width="100" height="100" className="rounded-circle" /> 
+                                <br/>
+                                <br/>
+                                <input type="string" value = {userInput.picture} onChange={(event: any) => setUserInput({...userInput, picture: event.target.value})}/>
+                                <br/>
+                                <label htmlFor="username">Username:</label>
+                                <br/>
+                                <input type="text" value={userInput.username} onChange={(event: any) => setUserInput({...userInput, username: event.target.value})}/>
+                                <br/>
+                                <label htmlFor="email">Email:</label>
+                                <br/>
+                                <input type="text" value={userInput.email} onChange={(event: any) => setUserInput({...userInput, email: event.target.value})}/>
+                                <br/>
+                                <label htmlFor="description">Description:</label>
+                                <br/>
+                                <textarea className='large-input' value = {userInput.description} onChange={(event: any) => setUserInput({...userInput, description: event.target.value})}/>
+                                <br/>
+                                <h6>{message}</h6>                            
+                                <button className = 'button' onClick={submitHandler}>Submit</button>                                   
+                            </form>
+                        </div>
+                    </div>  
+                </div>  
+            </div>
         </>    
     )
 }
