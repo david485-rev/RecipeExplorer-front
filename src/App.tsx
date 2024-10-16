cf2637733cd319444b25439e212e26438c7a6c50
David Ho <davidho6308493@gmail.com>
2024-10-16 11:28:13 -0700

    added error handling messages to register form

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import NavBar from './components/NavBar/NavBar';
 import {User, UserContext} from "./components/Context/UserContext"
 import LoginController from './components/Login/LoginController';
 import CreateRecipeController from './components/CreateRecipe/CreateRecipeController';
+import RegisterController from './components/Register/RegisterController';
 
 function App() : JSX.Element {
   const [user, setUser] = useState<User>({} as any)
@@ -19,7 +20,7 @@ function App() : JSX.Element {
       <Routes>
         <Route path="/MyProfile" element={<MyProfile/>}/>
         <Route path="/" element={<HomeController />}/>
-        <Route path="/register" element={<RegisterContainer/>}/>
+        <Route path="/register" element={<RegisterController/>}/>
         <Route path="/login" element={<LoginController setUser={setUser} />} />
         <Route path='/create-recipe' element={<CreateRecipeController />}/>
       </Routes>
