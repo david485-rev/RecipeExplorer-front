d0453675f708b745be116265c564574a02922c40
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-17 14:02:07 -0700

    Removed logic

--- src/components/CreateRecipe/CreateRecipeView.tsx
+++ src/components/CreateRecipe/CreateRecipeView.tsx
@@ -1,67 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import "../../styles/Recipes/CreateRecipeView.css"
 
-type FormProps = {submitForm: (formData: Object) => void, formMessage: string, submitted: boolean}
+type FormProps = {submitForm: (e: React.FormEvent<HTMLFormElement>, imageFile: File | undefined) => void, selectImage: (e: React.ChangeEvent<HTMLInputElement>) => void, imageFile: File | undefined, imageURL: string | undefined, formMessage: string, submitted: boolean}
 
-function CreateRecipeView({submitForm, formMessage, submitted}: FormProps) {
-  const [imageFile, setImageFile] = useState<File | undefined>(undefined);
-  const [imageFileURL, setImageFileURL] = useState<string | undefined>(undefined);
-  const [errorMessage, setErrorMessage] = useState<string>(formMessage);
-  const recipeFormRef = useRef<HTMLFormElement | null>(null);
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    try {
-      const file = e.target.files?.[0];
-      if (file) {
-        setErrorMessage("");
-        const imageUrl = URL.createObjectURL(file);
-        setImageFileURL(imageUrl);
-        setImageFile(file);
-      }
-    } catch(err) {
-      console.error(err);
-    }
-  }
-
-  function handleImageInsertion(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    try {
-      if (imageFile) {
-        const form = e.currentTarget;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-        formJson.recipeThumb = imageFile;
-        submitForm(formJson);
-      } else {
-        setErrorMessage("Image selection required");
-      }
-    } catch(err) {
-      console.error(err);
-    }
-  }
-
-  useEffect(() => {
-    if (submitted && recipeFormRef.current) {
-      recipeFormRef.current.reset();
-      setImageFile(undefined);
-      setImageFileURL(undefined);
-      setErrorMessage("");
-    }
-  }, [submitted]);
-
-  useEffect(() => {
-    return () => {
-      if (imageFileURL) {
-        URL.revokeObjectURL(imageFileURL);
-      }
-    };
-  }, [imageFileURL]);
-
+function CreateRecipeView({submitForm, selectImage, imageFile, imageURL, formMessage, submitted}: FormProps) {
   return (
     <div className='create-recipe-wrapper'>
       <h2>Create A New Recipe</h2>
-      <form ref={recipeFormRef} className="create-recipe-form" onSubmit={handleImageInsertion}>
+      <form className="create-recipe-form" onSubmit={(e: React.FormEvent<HTMLFormElement>) => submitForm(e, imageFile)}>
         <label>Recipe name
         <input type="text" id='recipe-name' name='recipeName' required/>
         </label>
@@ -88,13 +34,13 @@ function CreateRecipeView({submitForm, formMessage, submitted}: FormProps) {
 
         <div className="image-upload-wrapper">
           <label id="recipe-image-upload-label">Image
-          <input type="file" id='recipe-image-upload' name='recipeThumb' onChange={handleChange}/>
+          <input type="file" id='recipe-image-upload' name='recipeThumb' onChange={selectImage}/>
           </label>
-          {imageFileURL ? <img id='image-preview' src={imageFileURL} alt="preview" /> : ''}
+          {imageURL ? <img id='image-preview' src={imageURL} alt="preview" /> : ''}
         </div>
 
         <div className='recipe-form-message'>
-          <p>{errorMessage ? errorMessage : "" }</p>
+          <p>{formMessage ? formMessage : "" }</p>
         </div>
 
         <button type='submit'>{submitted ? "Recipe Submitted" : "Submit Recipe"}</button>
