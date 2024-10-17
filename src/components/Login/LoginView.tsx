f0bdc36fe3d17f16b1d86420e297ac59500f1cbf
David Ho <davidho6308493@gmail.com>
2024-10-17 14:04:59 -0700

    form shows missing username/password if either one is missing

--- src/components/Login/LoginView.tsx
+++ src/components/Login/LoginView.tsx
@@ -15,26 +15,63 @@ function LoginView(props: any) {
 
     function handleSubmit(event: any) {
         event.preventDefault();
+        console.log(userInput);
         props.login(userInput);
     }
 
-    function handleTestButton(event: any){
+    function handleTestButton(event: any) {
         event.preventDefault();
         console.log(user);
     }
 
+    function usernameInputHelp() {
+        return <>
+            <div className="mb-3">
+                <label htmlFor="inputUsername" className="form-label"></label>
+                <input type="text" name="username" placeholder='username' className="form-control is-invalid" id="inputUsername" value={userInput.username} onChange={(e: any) => setUserInput({ ...userInput, username: e.target.value })} />
+                <div id="serverUsernameFeedback" className="invalid-feedback">Please choose a username.</div>
+            </div>
+        </>
+    }
+
+    function passwordInputHelp() {
+        return <>
+            <div className="mb-3">
+                <label htmlFor="inputPassword" className="form-label"></label>
+                <input type='password' name="password" className="form-control is-invalid" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
+                <div id="serverPasswordFeedback" className="invalid-feedback">Please provide a password.</div>
+            </div>
+        </>
+    }
+
     function loginInputHelp(msg: string) {
-        switch(msg) {
+        switch (msg) {
+            case 'missing username':
+                return <>
+                    {usernameInputHelp()}
+                    <div className="mb-3">
+                        <label htmlFor="inputPassword" className="form-label"></label>
+                        <input type='password' name="password" className="form-control" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
+                    </div>
+                </>
+            case 'missing password':
+                return <>
+                    <div className="mb-3">
+                        <label htmlFor="inputUsername" className="form-label"></label>
+                        <input type='text' name="username" className="form-control" placeholder='username' onChange={(e: any) => setUserInput({ ...userInput, username: e.target.value })} />
+                    </div>
+                    {passwordInputHelp()}
+                </>
             case 'no account found':
                 return <>
                     <div className="mb-3">
                         <label htmlFor="inputUsername" className="form-label"></label>
-                        <input type="text" name="username" placeholder='username' className="form-control is-invalid" id="inputUsername" value={userInput.username} onChange={(e: any) => setUserInput({ ...userInput, username: e.target.value })}/>
+                        <input type="text" name="username" placeholder='username' className="form-control is-invalid" id="inputUsername" value={userInput.username} onChange={(e: any) => setUserInput({ ...userInput, username: e.target.value })} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="inputPassword" className="form-label"></label>
-                        <input type='password' className="form-control is-invalid" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
-                        <div id="serverUsernameFeedback" className="invalid-feedback">Invalid Credentials</div>
+                        <input type='password' name="password" className="form-control is-invalid" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
+                        <div id="serverFormFeedback" className="invalid-feedback">Invalid Credentials</div>
                     </div>
                 </>
             default:
@@ -45,7 +82,7 @@ function LoginView(props: any) {
                     </div>
                     <div className="mb-3">
                         <label htmlFor="inputPassword" className="form-label"></label>
-                        <input type='password' className="form-control" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
+                        <input type='password' name="password" className="form-control" placeholder='password' onChange={(e: any) => setUserInput({ ...userInput, password: e.target.value })} />
                     </div>
                 </>
         }
