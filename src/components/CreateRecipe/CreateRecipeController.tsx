28829a9ec4a4d911e38b0a9320c609dcd7a4c0f8
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-17 14:01:50 -0700

    Moved logic into controller

--- src/components/CreateRecipe/CreateRecipeController.tsx
+++ src/components/CreateRecipe/CreateRecipeController.tsx
@@ -3,27 +3,56 @@ import { useNavigate } from 'react-router-dom';
 import CreateRecipeView from './CreateRecipeView'
 import { UserContext } from '../Context/UserContext'
 import config from '../../config';
-const URL = `${config.path}`;
+const PURL = `${config.path}`;
 
 function CreateRecipeController() {
+  const [imageFile, setImageFile] = useState<File | undefined>(undefined);
+  const [imageFileURL, setImageFileURL] = useState<string | undefined>(undefined);
   const [submitted, setSubmitted] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
   const user = useContext(UserContext);
   const navigate = useNavigate();
 
-  function handleSubmit(formData: Object) {
+  function handleImageSelection(e: React.ChangeEvent<HTMLInputElement>) {
     try {
-      if (Object.values(formData).includes("")) {
-        setMessage("All fields are required");
-      } else {
-        createRecipe(formData);
+      const file = e.target.files?.[0];
+      
+      if (file) {
+        setMessage("");
+        const imageUrl = URL.createObjectURL(file);
+        setImageFileURL(imageUrl);
+        setImageFile(file);
       }
     } catch(err) {
       console.error(err);
     }
   }
 
-  async function createRecipe(recipe: Object) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>, imageFile: File | undefined) {
+    e.preventDefault();
+
+    try {
+      const form = e.currentTarget;
+      const formData = new FormData(form);
+      const formJson = Object.fromEntries(formData.entries());
+
+      if (formJson.recipeThumb instanceof File && formJson.recipeThumb.name === "" && imageFile) {
+        formJson.recipeThumb = imageFile;
+      }
+
+      if (Object.values(formJson).includes("")) {
+        setMessage("All fields are required");
+      } else if (formJson.recipeThumb instanceof File && formJson.recipeThumb.name === "") {
+        setMessage("You must upload an image")
+      } else {
+        createRecipe(formJson, form);
+      }
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  async function createRecipe(recipe: Object, formElement: HTMLFormElement) {
     const payload = {
       method: "POST",
       headers: {
@@ -34,10 +63,14 @@ function CreateRecipeController() {
     }
 
     try {
-      const recipeResponse = await fetch(`${URL}/recipes`, payload);
+      const recipeResponse = await fetch(`${PURL}/recipes`, payload);
       
       if (recipeResponse.ok) {
         setSubmitted(true);
+        formElement.reset();
+        setImageFile(undefined);
+        setImageFileURL(undefined);
+        setMessage("");
       }
 
       setTimeout(() => setSubmitted(false), 500);
@@ -48,13 +81,21 @@ function CreateRecipeController() {
   }
 
   useEffect(() => {
+    return () => {
+      if (imageFileURL) {
+        URL.revokeObjectURL(imageFileURL);
+      }
+    };
+  }, [imageFileURL]);
+
+  useEffect(() => {
     if (!user?.token) {
       navigate("/");
     }
   }, [user?.token, navigate]);
 
   return (
-    <CreateRecipeView submitForm={handleSubmit} formMessage={message} submitted={submitted}/>
+    <CreateRecipeView submitForm={handleSubmit} selectImage={handleImageSelection} imageFile={imageFile} imageURL={imageFileURL} formMessage={message} submitted={submitted}/>
   )
 }
 
